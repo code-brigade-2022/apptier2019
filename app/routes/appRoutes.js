9d77ab62bc890a7b55663fe8e185d4eb74399c60
mochi182 <mochi182tv@gmail.com>
2022-09-20 12:31:18 -0500

    kiss 6

--- app/routes/appRoutes.js
+++ app/routes/appRoutes.js
@@ -1,33 +1,13 @@
 module.exports = function(app) {
-  var Airports = require('../controller/appController.js');
-  var Distance = require('../controller/appController.js');
-
-  app.get('/envs', (req, res) => {
-    res.json({
-        node_env: process.env.NODE_ENV,
-        host: process.env.DB_HOST,
-        user: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
+    app.get('/', (req, res) => {
+        res.send('<h1>Hola</h1>')
     })
-  });
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  });
-
-  app.get('/airports', function(req, res) {
-    Airports.list_all_Airports(req, res);
-  });
-
-  app.get('/distances', function(req, res) {
-    console.log('from:', req.query.from);
-    console.log('to:', req.query.to);
-    Distance.list_all_Distances(req, res);
-  });
-
-  //app.route('/airports')
-  // .get(Airports.list_all_Airports, );
-  //app.route('/distances')
-  //  .get(Airports.list_all_distances, );
+    app.get('/airports', (req, res) => {
+        res.send('<h1>Airports</h1>')
+    })
+      
+    app.get('/distances', (req, res) => {
+        res.send('<h1>Distances</h1>')
+    })
 };
