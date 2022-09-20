c4c6f095c49284182cbc1074d8dc7ee64f016be4
mochi182 <mochi182tv@gmail.com>
2022-09-20 12:37:36 -0500

    kiss 7

--- app/routes/appRoutes.js
+++ app/routes/appRoutes.js
@@ -1,13 +1,27 @@
 module.exports = function(app) {
-    app.get('/', (req, res) => {
-        res.send('<h1>Hola</h1>')
-    })
+  var Airports = require('../controller/appController.js');
+  var Distance = require('../controller/appController.js');
 
-    app.get('/airports', (req, res) => {
-        res.send('<h1>Airports</h1>')
+  app.get('/envs', (req, res) => {
+    res.json({
+        node_env: process.env.NODE_ENV,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     })
-      
-    app.get('/distances', (req, res) => {
-        res.send('<h1>Distances</h1>')
-    })
+  });
+
+  app.get('/', (req, res) => {
+    res.send('Hola')
+  });
+
+  app.get('/airports', function(req, res) {
+    res.send('Hola')
+  });
+
+  app.get('/distances', function(req, res) {
+    res.send('Hola')
+  });
+
 };
