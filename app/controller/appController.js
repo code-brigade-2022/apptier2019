45f79dec41b9d4e12470c2adc9803ba69f0c2e05
mochi182 <mochi182tv@gmail.com>
2022-09-20 12:26:46 -0500

    kiss 5

--- app/controller/appController.js
+++ app/controller/appController.js
@@ -1,5 +1,3 @@
-'use strict';
-
 var Airport = require('../models/appModel.js').Airport;
 var Distance = require('../models/appModel.js').Distance;
 
@@ -20,54 +18,3 @@ exports.list_all_Distances = function(req, res) {
     res.json(Distance);
   });
 };
-
-/*
-exports.create_a_Airport = function(req, res) {
-  var new_Airport = new Airport(req.body);
-
-  //handles null error 
-   if(!new_Airport.Airport || !new_Airport.status){
-
-            res.status(400).send({ error:true, message: 'Please provide Airport/status' });
-
-        }
-else{
-  
-  Airport.createAirport(new_Airport, function(err, Airport) {
-    
-    if (err)
-      res.send(err);
-    res.json(Airport);
-  });
-}
-};
-
-
-exports.read_a_Airport = function(req, res) {
-  Airport.getAirportById(req.params.AirportId, function(err, Airport) {
-    if (err)
-      res.send(err);
-    res.json(Airport);
-  });
-};
-
-
-exports.update_a_Airport = function(req, res) {
-  Airport.updateById(req.params.AirportId, new Airport(req.body), function(err, Airport) {
-    if (err)
-      res.send(err);
-    res.json(Airport);
-  });
-};
-
-
-exports.delete_a_Airport = function(req, res) {
-
-
-  Airport.remove( req.params.AirportId, function(err, Airport) {
-    if (err)
-      res.send(err);
-    res.json({ message: 'Airport successfully deleted' });
-  });
-}
-*/
