5ca5a2eeeb3de99d94a036732071c6c4dba02db1
mochi182 <mochi182tv@gmail.com>
2022-09-20 11:53:13 -0500

    kiss

--- app/models/db.js
+++ app/models/db.js
@@ -3,46 +3,6 @@
 var sql;
 var connection;
 
-/* if (process.env.NODE_ENV == "development"){
-
-    sql = require('mysql');
-    //grant all privileges on copaDataSet.* to admin@'%' identified by 'copadb2020';
-    //local mysql db connection
-    var connection = sql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
-    });
-
-    connection.connect(function(err) {
-        if (err) throw err;
-    });
-} else {
-    const config = {
-        user: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        server: process.env.DB_SERVER,
-        port: parseInt(process.env.DB_PORT),
-        database: process.env.DB_DATABASE,
-        connectionTimeout: 3000,
-        parseJSON: true,
-        options: {
-          encrypt: true,
-          enableArithAbort: true
-        },
-        pool: {
-          min: 0,
-          idleTimeoutMillis: 3000
-        }
-    };
-    
-    sql = require('mssql')
-    const pool = new sql.ConnectionPool(config);
-    connection = pool.connect(); 
-}
- */
-
 sql = require('mysql');
 //grant all privileges on copaDataSet.* to admin@'%' identified by 'copadb2020';
 //local mysql db connection
--- server.js
+++ server.js
@@ -1,41 +1,10 @@
 const express = require('express');
 app = express();
-bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
-
-// Configurar variables de entorno (.env):
-const fs = require("fs");
-const env_file = './env/.env';
-if (fs.existsSync(env_file)){
-    const dotenv = require('dotenv');
-    const dotenv_path = './env/.env';
-    dotenv.config({path: dotenv_path});
-}
-
-// CORS:
-/* var cors = require('cors');
-app.use(cors());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-
-    // authorized headers for preflight requests
-    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-
-    app.options('*', (req, res) => {
-        // allowed XHR methods  
-        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-        res.send();
-    });
-}); */
 
 app.listen(port);
 
 console.log('API server started on: ' + port);
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 var routes = require('./app/routes/appRoutes.js'); //importing route
 routes(app); //register the route
